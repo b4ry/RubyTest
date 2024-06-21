e9f816f397c26748a1912244fd774bc056c40e8d
b4ry <barry90@interia.pl>
2024-06-21 17:17:19 +0200

    - a simple thread-safe fixed-window rate limiter

--- main.rb
+++ main.rb
@@ -1,39 +1,85 @@
-class Greeter
-    attr_accessor :names
+$stdout.sync = true
 
-    def initialize(names = "World")
-        @names = names
+class RateLimiter
+    CAPACITY = 5
+    TIME_WINDOW = 3
+
+    def initialize
+        @queue = []
+        @mutex = Mutex.new
+        @convar = ConditionVariable.new
     end
 
-    def sayHello
-        if @names.nil?
-            puts "..."
-        elsif @names.respond_to?("each")
-            @names.each do |name|
-                puts "Hello, #{name}!"
+    def enqueue(request)
+        @mutex.synchronize do
+            while @queue.count >= CAPACITY
+                @convar.wait(@mutex)
             end
-        else
-            puts "Hello, #{names}!"
+
+            request.set_time_to_now
+            @queue.unshift(request)
         end
     end
 
-    def sayGoodBye
-        if @names.nil?
-            puts "..."
-        elsif @names.respond_to?("join")
-            puts "Bye, #{@names.join(", ")}!"
-        else
-            puts "Bye, #{@names}!"
+    def dequeue
+        loop do
+            if @queue.count > 0 && @queue.last.get_time + TIME_WINDOW < Time.now.to_i
+                @mutex.synchronize do
+                    puts "Removing request #{@queue.last}, because #{@queue.last.get_time} < #{Time.now.to_i}"
+                    @queue.pop
+                    @convar.signal
+                end
+            end
         end
     end
 end
 
-if __FILE__ == $0
-    g = Greeter.new("Luke")
-    g.sayHello()
-    g.sayGoodBye()
+class Request
+    @@id = 0
 
-    g = Greeter.new("Luke, Julieta")
-    g.sayHello()
-    g.sayGoodBye()
+    def initialize
+        @id = @@id
+        @@id += 1
+    end
+
+    def set_time_to_now
+        @time = Time.now.to_i
+    end
+
+    def get_time
+        @time
+    end
+
+    def to_s
+        @id.to_s
+    end
+end
+
+if __FILE__ == $0
+    rateLimiter = RateLimiter.new
+    requests = []
+
+    producer = Thread.new do
+        puts "Producing requests..."
+
+        100.times do
+            requests << Request.new
+        end
+
+        puts "Finish requests production: #{requests.count} requests"
+        puts "Enqueing requests..."
+
+        requests.each do |r|
+            rateLimiter.enqueue(r)
+            puts "Enqueued request #{r}"
+            sleep(rand(3))
+        end
+    end
+
+    consumer = Thread.new do
+        rateLimiter.dequeue
+    end
+
+    producer.join
+    consumer.kill
 end
